f21a6e814ce2594dc4b07cff6d9643340bf29ce4
asp <asp@aptech.com>
2026-09-25 09:59:37 +0500

    Add color script and dependencies

---
+++ color.js
@@ -0,0 +1,9 @@
+const chalk = require ("chalk")
+
+console.log(chalk.green("Good Morning"));
+console.log(chalk.red("Ladies and Gentalman, crazy hogaya"));
+console.log(chalk.blue("Hello World"));
+
+console.log(chalk.white("Hello World"));
+console.log(chalk.black("Hello World"));
+console.log(chalk.black("Hello World"));
